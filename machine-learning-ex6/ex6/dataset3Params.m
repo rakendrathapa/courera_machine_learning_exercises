2f55fffb3b24dd38ee3026f29c36019439318827
Rakendra Thapa <rakendrathapa@gmail.com>
2018-03-20 01:08:42 +0530

    Completed Week 7 Exercises.

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -22,13 +22,36 @@ sigma = 0.3;
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
 %
+list_val = [0.01 0.03 0.1 0.3 1.0 3.0 10.0 30.0];
+err_mean = 1.0;     % MAX error value between y and yval
+C_sigma_list = [0 0 0];
 
+for i =1:length(list_val),
+  C = list_val(i);
+  for j=1:length(list_val),
+    sigma = list_val(j);
+    %fprintf('C=%f sigma=%f\n', C, sigma);
+    
+    model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma)); 
+    predictions = svmPredict(model, Xval);
+    
+    prediction_err_mean = mean(double(predictions ~= yval));
+    %fprintf('prediction_err_mean=%f err_mean=%f\n', prediction_err_mean, err_mean);
+    
+    if prediction_err_mean <= err_mean,
+      %fprintf('prediction_err_mean < err_mean\n');
+      err_mean = prediction_err_mean;
+      C_val = C;
+      sigma_val = sigma;
+      
+      C_sigma_list = [C_sigma_list;prediction_err_mean C_val sigma_val];
+    end;    
+  end;
+end;
 
-
-
-
-
-
+%C_sigma_list(2:end, :)
+C = C_val;
+sigma = sigma_val;
 % =========================================================================
 
 end
